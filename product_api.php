0f7180044466bdd49dd61735f38bf9e64aa4ba38
rajataghi <rajataghi@gmail.com>
2014-01-29 01:08:08 +0530

    corrected memcached and spacing

--- product_api.php
+++ product_api.php
@@ -1,166 +1,188 @@
 <?php
+	class API
+	{
+		public
+		function __construct()
+		{ //connect to the database
+			mysql_connect('localhost','root','') or die("cannot connect");
+			mysql_select_db('thedroidpeople') or die("cannot select");
+			$mc= new Memcache();
+			$mc->connect('localhost', 11211);
+		}
+        
+		/*
+		 * Public method for accessing the api.
+		 * This method calls the method based on the query string
+		 *
+		 */
+		 
+		public
+		function process_api()
+		{   
+			
+			if(function_exists($_GET['method']))
+			{
+				$_GET['method']() ;
+			}
+			else     echo "function not found" ;
+		}
 
-class API {
-//connect to db
-$memcache = new Memcache() ;                
+		public
+		function get_request_method()
+		{
+			return $_SERVER['REQUEST_METHOD'];
+		}
 
-mysql_connect('localhost','root','') or die("cannot connect");
-
-mysql_select_db('thedroidpeople') or die("cannot select");
-
-//check if the called function exists. if yes, call it.
-
-process_api(){
-if(function_exists($_GET['method']))
-  { 
-   $_GET['method']() ;             //the method specified in the url call to the api
-  
-  }
-  else
-     echo "function not found" ;
-}	 
-
-public function get_request_method(){  //returns the type of request made
-	
-	return $_SERVER['REQUEST_METHOD'];
-	
-	}
-		
-		
-private function cleanInputs($data){              //this method is used to extract information about a particular product(used for search and deletion)
-			$clean_input = array();               
-			if(is_array($data)){
-				foreach($data as $k => $v){
+		private
+		function cleanInputs($data)
+		{
+			$clean_input = array();
+			
+			if(is_array($data))
+			{
+				foreach($data as $k => $v)
+				{
 					$clean_input[$k] = $this->cleanInputs($v);
 				}
-			}else{
-				if(get_magic_quotes_gpc()){
-					$data = trim(stripslashes($data));                      // Found it on stackoverflow, as I did not know how to do this part.
+
+			}
+			else
+			{
+				
+				if(get_magic_quotes_gpc())
+				{
+					$data = trim(stripslashes($data));
 				}
+
 				$data = strip_tags($data);
 				$clean_input = trim($data);
 			}
+
 			return $clean_input;
-		}				
-		
-		
-//methods
-private function login() {
-
- $email = $_POST['email'] ;
- $password = $_POST['password'] ;  //get login credentials of the user from the login form
- $user_details=array() ;
-
- //basic validation
- if(!empty($email) and !empty($password)) {
-    
-    $sql= "SELECT * from members WHERE email='$email' and pass='$password'" ; //members table contains info for all authenticated members
-   
-   if(mysql_num_rows($sql) > 0) {                       
-        $user_details = mysql_fetch_array($sql,MYSQL_ASSOC);
-  
-        $user_details=json_encode($user_details) ;       //user details in json format
-		return $user_details ;
-      
-	  }
-	else
-		echo "invalid email address or password" ;
-else 
-   echo "email and/or password field is empty" ;
- }
-}
-
-
-private function getallproducts(){
-
-  if($this->get_request_method() != "GET"){         //confirm whether the request method is get otherwise no data is retrieved
-		
-		echo "incorrect request method" ;
-			
-	}
-  else {			
-        $key=md5("select * from products")         //for memcache
-		$q=mysql_query("select * from products");  //products table contains all product related information(mentioned in the design doc.)
-
-		$products=array() ;
-
-		while($p=mysql_fetch_array($q,MYSQL_ASSOC))
-			{	 
-				$products[]=$p ;
-   
-			}
-  
-  $products=json_encode($products);                       //list of all products in json format
-  $memcache->set($key,$products,TRUE,500) ;              //cache the result for 500 seconds
-  return $products ;
-        
 		}
- }
- 
- private function deleteproduct(){
-			                                               // confirm the type of request
-	if($this->get_request_method() != "DELETE"){
+
+		private
+		function login()
+		{
+			$email = $_POST['email'] ;
+			$password = $_POST['password'] ;
+			$user_details=array() ;
+			
+			if(!empty($email) and !empty($password))
+			{
+				$sql= "SELECT * from members WHERE email='$email' and pass='$password'" ;
+				
+				if(mysql_num_rows($sql) > 0)
+				{
+					$user_details = mysql_fetch_array($sql,MYSQL_ASSOC);
+					$user_details=json_encode($user_details) ;
+					return $user_details ;
+				}
+				else echo "invalid email address or password" ;
+				else    echo "email and/or password field is empty" ;
+			}
+
+		}
+
+		private
+		function getallproducts()
+		{
+			
+			if($this->get_request_method() != "GET")
+			{
 				echo "incorrect request method" ;
 			}
-	else {
+			else
+			{
+				$products=array() ;
+				$products = $mc->get("products") ;
+				
+				if($products === false)
+				{
+					$q=mysql_query("select * from products");
+					while($p=mysql_fetch_array($q,MYSQL_ASSOC))
+					{
+						$products[]=$p ;
+					}
+
+					$mc->set("products",serialize($products),0,100) ;
+				}
+				else
+				{
+					$products2= unserialize($products) ;
+					$products=$products2 ;
+				}
+
+				$products=json_encode($products);
+				return $products ;
+			}
+
+		}
+
+		private
+		function deleteproduct()
+		{
 			
-		$del=$this->cleanInputs($_GET);
-		$id = (int)$this->$del['id'];               //get the id of the product to be deleted.
-		if($id > 0) {
-		
-		mysql_query("DELETE from products where id = $id") ;  //compare with id field in the database
-		echo "record successfully deleted" ;
-		
-		}
-		
-		else
-			  echo "no record with this id" ;
-		}
- }
- 
- private function getproduct(){                         //retrieve info about a single product
-	if($this->get_request_method() != "GET"){         
+			if($this->get_request_method() != "DELETE")
+			{
 				echo "incorrect request method" ;
 			}
-    else {
-	
-		$del = $this->cleanInputs($_GET);
-	    $id = (int)$this->$del['id'];               //get the id of product to be found.
-        if($id > 0) {
-			$search_query=mysql_query("SELECT * from products where id = $id") ;	
-			
-			$search_result = mysql_fetch_array($search_query,MYSQL_ASSOC);
-  
-            $search_result=json_encode($search_result) ;
-			return $search_result ;                         //return product info in json format
+			else
+			{
+				$del=$this->cleanInputs($_GET);
+				$id = (int)$this->$del['id'];
+				
+				if($id > 0)
+				{
+					mysql_query("DELETE from products where id = $id") ;
+					echo "record successfully deleted" ;
+				}
+				else  echo "no record with this id" ;
+			}
+
 		}
-		else 
-			echo "no product with this id" ;
-	} 
- }
- 
- private function addproduct(){                        //function to add a new product to the database         
-                                                       /* name,info,barcode,tags are "form" fields which give info about the product to be added
-                                                          function to be called when form is submitted
-													   */
-	$Name = json_decode($_POST['name']);												   
-	$Info = json_decode($_POST['info']);
-    $Barcode = json_decode($_POST['barcode']);
-	$Tags = json_decode($_POST['tags']);
-    	
-    $insert_query = "INSERT into products(id,name,info,barcode,tags) 
+
+		private
+		function getproduct()
+		{
+			
+			if($this->get_request_method() != "GET")
+			{
+				echo "incorrect request method" ;
+			}
+			else
+			{
+				$del = $this->cleanInputs($_GET);
+				$id = (int)$this->$del['id'];
+				
+				if($id > 0)
+				{
+					$search_query=mysql_query("SELECT * from products where id = $id") ;
+					$search_result = mysql_fetch_array($search_query,MYSQL_ASSOC);
+					$search_result=json_encode($search_result) ;
+					return $search_result ;
+				}
+				else echo "no product with this id" ;
+			}
+
+		}
+
+		private
+		function addproduct()
+		{
+			$Name = json_decode($_POST['name']);
+			$Info = json_decode($_POST['info']);
+			$Barcode = json_decode($_POST['barcode']);
+			$Tags = json_decode($_POST['tags']);
+			$insert_query = "INSERT into products(id,name,info,barcode,tags) 
 	                     values('','$Name','$Info','$Barcode','$Tags')";
-    $execute_query = mysql_query($insert_query);
+			$execute_query = mysql_query($insert_query);
+			echo "Record successfully inserted" ;
+		}
+
+	}
+    // Initiate Library
 	
-	echo "Record successfully inserted" ;
- 
- }
- 
-
- }
-
-
-$api = new API;        //initialize the API class
-$api->process_api();
-
-?>
+	$api = new API;
+	$api->process_api();
+	?>
